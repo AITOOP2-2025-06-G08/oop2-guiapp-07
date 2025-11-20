918ff78d96b3d96707e7ca8934c29087028e86ea
告白 <kout5504@gmail.com>
2025-11-20 13:06:26 +0900

    Update lecture04_main.py

--- lecture04_main.py
+++ lecture04_main.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.abspath(__file__))
     audio_file = os.path.join(script_dir, audio_file)
     # 録音
-    #record.record_audio(audio_file, record_seconds = 10)
+    record.record_audio(audio_file, record_seconds = 10)
     # 文字起こし結果を保存
     save.save_transcription_to_file(audio_file, "transcription.txt")
